78c286fd364d8b5cd76619f73f65d355a0397f3f
pampas93 <abhijit.93@hotmail.com>
2019-11-24 13:40:15 -0700

    Creating a GLFW window

--- OpenGLMaiden/OpenGLMaiden/main.cpp
+++ OpenGLMaiden/OpenGLMaiden/main.cpp
@@ -1,4 +1,69 @@
+#include <stdio.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 800;
+const GLint HEIGHT = 600;
+
 int main()
 {
+  // Initialize GLFW
+  if (!glfwInit()) {
+    printf("GLFW initialization failed");
+    glfwTerminate();
+    return 1;
+  }
+
+  // Setup GLFW window properties
+  // OpenGL version
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  // Core profile = No Backward compatibility
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+  // Creating the GLFW window
+  GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Maiden OpenGL", NULL, NULL);
+  if (!mainWindow) {
+    printf("GLFW window creation failed");
+    glfwTerminate();
+    return 1;
+  }
+
+  // Get buffer size information
+  int bufferWidth, bufferHeight;
+  glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+  // Set context for GLEW to use
+  glfwMakeContextCurrent(mainWindow);
+
+  // Allow modern extension features
+  glewExperimental = GL_TRUE;
+
+  if (glewInit() != GLEW_OK) {
+    printf("GLEW initialization failed");
+    glfwDestroyWindow(mainWindow);
+    glfwTerminate();
+    return 1;
+  }
+
+  // Setup viewport size
+  glViewport(0, 0, bufferWidth, bufferHeight);
+
+  while (!glfwWindowShouldClose(mainWindow)) {
+    
+    // Get & Handle user input events
+    glfwPollEvents();
+
+    // Clear window
+    glClearColor(0.6f, 0.1f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Swap the buffers from where we drew to what we see
+    glfwSwapBuffers(mainWindow);
+  }
+
   return 0;
 }
